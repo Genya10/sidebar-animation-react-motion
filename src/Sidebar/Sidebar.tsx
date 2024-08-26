71845732abd15afe1980340050fa334cc01de3a5
Jenya <113167825+Genya10@users.noreply.github.com>
2024-08-26 19:50:27 +0300

    install and use framer-motion and clsx. Also create style collapsed

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -2,7 +2,9 @@ import { Menu } from "./menu/Menu"
 import cl from './Sidebar.module.scss'
 import { useAtom } from "jotai/react";
 import { PanelLeftClose, PanelLeftOpen } from 'lucide-react'
-import { isCollapsedAtom } from "../store";
+import { isCollapsedAtom } from "../store"
+import { m } from "framer-motion"
+import cn from 'clsx'
 
 export function Sidebar(){
     const [isCollapsed, setIsCollapsed] = useAtom(isCollapsedAtom);
@@ -12,13 +14,18 @@ export function Sidebar(){
     }
 
     return (
-        <aside className={cl.sidebar}>
+        <m.aside className={cn(cl.sidebar,{
+            [cl.collapsed]: isCollapsed
+        })}
+           animate={{width: isCollapsed ? 50 : 208}}
+           transition={{type:'spring', stiffness: 300, damping:20}}
+        >
             <button 
               className = {cl.toggle}
               onClick = {toggleSidebar}>
             {isCollapsed ? <PanelLeftOpen/> : <PanelLeftClose/>}
             </button>
             <Menu />
-        </aside>
+        </m.aside>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { LazyMotion , domAnimation } from 'framer-motion'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LazyMotion features={domAnimation}>
+      <App />
+    </LazyMotion>    
   </StrictMode>,
 )
