13818d197d923d1301fc3ce6482ce394dd82b7b3
Jenya <113167825+Genya10@users.noreply.github.com>
2024-08-26 17:40:48 +0300

    create state isCollapsed to Sidebar and add to it styles

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -1,9 +1,22 @@
 import { Menu } from "./menu/Menu"
 import cl from './Sidebar.module.scss'
+import { useState } from "react"
+import { PanelLeftClose, PanelLeftOpen } from 'lucide-react'
 
 export function Sidebar(){
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsCollapsed(!isCollapsed)
+    }
+
     return (
         <aside className={cl.sidebar}>
+            <button 
+              className = {cl.toggle}
+              onClick = {toggleSidebar}>
+            {isCollapsed ? <PanelLeftOpen/> : <PanelLeftClose/>}
+            </button>
             <Menu />
         </aside>
     )
