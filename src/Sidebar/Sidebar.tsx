7bb5e843d77c567d57548594e8ed4d86e7b606ab
Jenya <113167825+Genya10@users.noreply.github.com>
2024-08-27 17:36:12 +0300

    create state isCanHover and hoverTimeoutRef. Also add styles

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -1,31 +1,46 @@
-import { Menu } from "./menu/Menu"
-import cl from './Sidebar.module.scss'
+import { Menu } from "./menu/Menu";
+import cl from "./Sidebar.module.scss";
 import { useAtom } from "jotai/react";
-import { PanelLeftClose, PanelLeftOpen } from 'lucide-react'
-import { isCollapsedAtom } from "../store"
-import { m } from "framer-motion"
-import cn from 'clsx'
+import { PanelLeftClose, PanelLeftOpen } from "lucide-react";
+import { isCollapsedAtom } from "../store";
+import { m } from "framer-motion";
+import cn from "clsx";
+import { useRef, useState } from "react";
 
-export function Sidebar(){
-    const [isCollapsed, setIsCollapsed] = useAtom(isCollapsedAtom);
+export function Sidebar() {
+  const [isCollapsed, setIsCollapsed] = useAtom(isCollapsedAtom);
+  const [isCanHover, setIsCanHover] = useState(true);
+  const hoverTimeoutRef = useRef<number | null>(null);
 
-    const toggleSidebar = () => {
-        setIsCollapsed(!isCollapsed)
+  const toggleSidebar = () => {
+    if (isCollapsed) {
+      setIsCanHover(false);
+
+      if (hoverTimeoutRef.current) {
+        clearTimeout(hoverTimeoutRef.current);
+      }
+
+      hoverTimeoutRef.current = setTimeout(() => {
+        setIsCanHover(true);
+      }, 500);
     }
+    console.log(isCanHover)
+    setIsCollapsed(!isCollapsed);
+  };
 
-    return (
-        <m.aside className={cn(cl.sidebar,{
-            [cl.collapsed]: isCollapsed
-        })}
-           animate={{width: isCollapsed ? 50 : 208}}
-           transition={{type:'spring', stiffness: 300, damping:20}}
-        >
-            <button 
-              className = {cl.toggle}
-              onClick = {toggleSidebar}>
-            {isCollapsed ? <PanelLeftOpen/> : <PanelLeftClose/>}
-            </button>
-            <Menu />
-        </m.aside>
-    )
+  return (
+    <m.aside
+      className={cn(cl.sidebar, {
+        [cl.collapsed]: isCollapsed,
+        [cl.canHover]: isCanHover,
+      })}
+      animate={{ width: isCollapsed ? 50 : 208 }}
+      transition={{ type: "spring", stiffness: 300, damping: 25 }}
+    >
+      <button className={cl.toggle} onClick={toggleSidebar}>
+        {isCollapsed ? <PanelLeftOpen /> : <PanelLeftClose />}
+      </button>
+      <Menu />
+    </m.aside>
+  );
 }
